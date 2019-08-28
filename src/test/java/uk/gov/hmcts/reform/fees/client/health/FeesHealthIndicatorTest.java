81eedcaa81772bde6e750bc69068b1149f092708
Andrew Walker <andrewwa@kainos.com>
2019-08-28 11:21:40 +0100

    Add health indicator test

--- src/test/java/uk/gov/hmcts/reform/fees/client/health/FeesHealthIndicatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/fees/client/health/FeesHealthIndicatorTest.java
@@ -1,4 +1,31 @@
 package uk.gov.hmcts.reform.fees.client.health;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.fees.client.CoreFeignConfiguration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = {FeesHealthIndicator.class, CoreFeignConfiguration.class})
+@ExtendWith(SpringExtension.class)
+@EnableAutoConfiguration
+@AutoConfigureWireMock(port = 8091)
 class FeesHealthIndicatorTest {
+    @Autowired
+    private FeesHealthIndicator healthIndicator;
+
+    @Test
+    void testHealthCheck() {
+        Health health = healthIndicator.health();
+        assertNotNull(health);
+        assertEquals(Status.UP, health.getStatus());
+    }
 }
