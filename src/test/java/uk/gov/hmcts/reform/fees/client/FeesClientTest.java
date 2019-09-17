546c44bf48b04a29c7696d80d944a9c08b206f8c
Andrew Walker <andrewwa@kainos.com>
2019-09-17 11:55:00 +0100

    Add failure scenarios to unit tests

--- src/test/java/uk/gov/hmcts/reform/fees/client/FeesClientTest.java
+++ src/test/java/uk/gov/hmcts/reform/fees/client/FeesClientTest.java
@@ -1,7 +1,11 @@
 package uk.gov.hmcts.reform.fees.client;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +18,12 @@ import uk.gov.hmcts.reform.fees.client.model.FeeVersion;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = {FeesClient.class, FeesApi.class, CoreFeignConfiguration.class})
 @ExtendWith(SpringExtension.class)
@@ -104,5 +113,67 @@ class FeesClientTest {
             () -> assertThat(result.getFlatAmount().getAmount().doubleValue()).isEqualTo(amount),
             () -> assertThat(result.getMemoLine()).isEqualTo(memoLine)
         );
+    }
+
+    @Nested
+    @DisplayName("Propagation")
+    class Propagation {
+        @Mock private FeesApi feesApi;
+
+        @BeforeEach
+        void replaceFeesClient() {
+            feesClient = new FeesClient(feesApi, "my service", "jurisdiction one", "jurisdiction two");
+        }
+
+        @Test
+        void lookupFeeShouldInvokeFeesApi() {
+            feesClient.lookupFee("my channel", "my event", BigDecimal.valueOf(999.99));
+            verify(feesApi).lookupFee(
+                "my service",
+                "jurisdiction one",
+                "jurisdiction two",
+                "my channel",
+                "my event",
+                BigDecimal.valueOf(999.99)
+            );
+        }
+
+        @Test
+        void findRangeGroupShouldInvokeFeesApi() {
+            feesClient.findRangeGroup("my channel", "my event");
+            verify(feesApi).findRangeGroup(
+                "my service",
+                "jurisdiction one",
+                "jurisdiction two",
+                "my channel",
+                "my event"
+            );
+        }
+
+        @Test
+        void lookupFeeShouldPropagateExceptions() {
+            when(feesApi.lookupFee(
+                anyString(), anyString(), anyString(), anyString(), anyString(), any(BigDecimal.class)
+            )).thenThrow(
+                new RuntimeException("expected exception for lookupFee")
+            );
+
+            assertThatThrownBy(() -> feesClient.lookupFee("my channel", "my event", BigDecimal.valueOf(999.99)))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("expected exception for lookupFee");
+        }
+
+        @Test
+        void findRangeGroupShouldPropagateExceptions() {
+            when(
+                feesApi.findRangeGroup(anyString(), anyString(), anyString(), anyString(), anyString())
+            ).thenThrow(
+                new RuntimeException("expected exception for findRangeGroup")
+            );
+
+            assertThatThrownBy(() -> feesClient.findRangeGroup("my channel", "my event"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("expected exception for findRangeGroup");
+        }
     }
 }
--- src/test/java/uk/gov/hmcts/reform/fees/client/health/FeesHealthIndicatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/fees/client/health/FeesHealthIndicatorTest.java
@@ -8,9 +8,16 @@ import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.fees.client.CoreFeignConfiguration;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -23,9 +30,36 @@ class FeesHealthIndicatorTest {
     private FeesHealthIndicator healthIndicator;
 
     @Test
-    void testHealthCheck() {
+    void testHealthCheckUp() {
+        stubFor(get(urlEqualTo("/health/liveness"))
+            .willReturn(aResponse()
+                .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .withBody("{\"status\":\"UP\"}")));
+
         Health health = healthIndicator.health();
         assertNotNull(health);
         assertEquals(Status.UP, health.getStatus());
+    }
+
+    @Test
+    void testHealthCheckDown() {
+        stubFor(get(urlEqualTo("/health/liveness"))
+            .willReturn(aResponse()
+                .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .withBody("{\"status\":\"DOWN\"}")));
+
+        Health health = healthIndicator.health();
+        assertNotNull(health);
+        assertEquals(Status.DOWN, health.getStatus());
+    }
+
+    @Test
+    void testFailedHealthCheck() {
+        stubFor(get(urlEqualTo("/health/liveness"))
+            .willReturn(aResponse().withStatus(HttpStatus.BAD_REQUEST.value())));
+
+        Health health = healthIndicator.health();
+        assertNotNull(health);
+        assertEquals(Status.DOWN, health.getStatus());
     }
 }
